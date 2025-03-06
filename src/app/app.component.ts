ca986485482a0ae8125de18a3fdcfe38c737329a
Lizden09 <lruanom5@miumg.edu.gt>
2025-03-06 17:02:14 -0600

    Descripcion y habilidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from "./skills/skills.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkillsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
