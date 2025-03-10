d5c55f6d6d7bae53d65810765704943c140c592c
Lizden09 <lruanom5@miumg.edu.gt>
2025-03-10 15:47:38 -0600

    Implementacion Carrusel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SkillsComponent } from "./skills/skills.component";
 import { CommonModule } from '@angular/common';
+import { ProyectsComponent } from "./proyects/proyects.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SkillsComponent, CommonModule],
+  imports: [RouterOutlet, SkillsComponent, CommonModule, ProyectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -1,11 +1,80 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-proyects',
-  imports: [],
+  imports: [SlickCarouselModule, CommonModule],
   templateUrl: './proyects.component.html',
   styleUrl: './proyects.component.css'
 })
 export class ProyectsComponent {
+  slides = [
+    { 
+      title: 'Dashboard', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'Proyectos', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'Reportes', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'hola', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'adios', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'Tienda', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    },
+    { 
+      title: 'POS', 
+      image: '../../assets/images/cafe.jpg'  // Ajusta la URL de la imagen
+    }
+  ];
 
+  slideConfig = {
+    "slidesToShow": 3,
+    "slidesToScroll": 1,
+    "autoplay": true,
+    "autoplaySpeed": 2000,
+    "infinite":true,
+    "pauseOnHover": true
+  }
+  // slideconfig = {
+  //   "slidesToShow": 4,
+  //   "slidesToScroll": 4,
+  //   "autoplay": true,
+  //   "autoplaySpeed": 5000,
+  //   "pauseOnHover": true,
+  //   "infinite": true,
+  //   "responsive": [
+  //     {
+  //       "breakpoint":992,
+  //       "settings": {
+  //         "arrows": true,
+  //         "infinite" : true,
+  //         "slidesToShow": 3,
+  //         "slidesToScroll": 3
+  //       }
+  //     },
+  //     {
+  //       "breakpoint":768,
+  //       "settings":{
+  //         "arrows": true,
+  //         "infinite": true,
+  //         "slidesToShow": 1,
+  //         "slidesToScroll": 1,
+  //       }
+  //     }
+  //   ]
+  // }
 }
+
